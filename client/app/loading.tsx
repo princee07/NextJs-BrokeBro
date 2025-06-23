501bc9d49e79ef205a6cc505734f3ccf565f2ff1
Lavavarshney <lavanya.varshney2104@gmail.com>
2025-06-23 11:17:59 +0530

    authentication done

---
+++ client/app/loading.tsx
@@ -0,0 +1,8 @@
+ function Loading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="animate-spin rounded-full border-4 border-solid border-primary-500 border-t-transparent h-16 w-16"></div>
+    </div>
+  );
+}
+export default Loading;
---
+++ client/app/not-found.tsx
@@ -0,0 +1,11 @@
+function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">404</h1>
+        <p className="mt-4 text-lg">Page Not Found</p>
+      </div>
+    </div>
+  );
+}
+export default NotFound;
