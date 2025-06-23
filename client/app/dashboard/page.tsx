6317c27d606df362ccc151e4bdf86e4c6f1bec80
Lavavarshney <lavanya.varshney2104@gmail.com>
2025-06-23 14:10:30 +0530

    review section routes done

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,17 +1,31 @@
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import Image from "next/image";
 export default async function Dashboard() {
     const {getUser} = getKindeServerSession();
 const user = await getUser();
+console.log(user);
+console.log("Profile Picture URL:", user?.picture);
+
   return (
     <div>
       <h1>This is Dashboard</h1>
       <p>Welcome to the dashboard!</p>
       <p>Here you can manage your account and settings.</p>
-      <div>Hey {user?.given_name}
+      <div>Welcome, {user?.given_name} </div>
+      <div>
+  {user?.picture && (
+    <Image
+      src={user.picture}
+      alt="Profile"
+      width={96}
+      height={96}
+    />
+  )}
+</div>
         <LogoutLink>Sign Out</LogoutLink>
       </div>
-    </div>
+
   );
 }
 
