409c93f150d1d0c6e7a2e86a8fd7da0b5cbe4879
Lavavarshney <lavanya.varshney2104@gmail.com>
2025-07-05 14:26:36 +0530

    Add Google Analytics to layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,7 +3,7 @@ import "./styles/global.css";
 import NavbarWrapper from "@/components/layout/NavbarWrapper";
 import Footer from "@/components/layout/Footer";
 import ReferralProcessor from "@/components/auth/ReferralProcessor";
-import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -18,6 +18,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+            <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-M844TQKBMV"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M844TQKBMV');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <ReferralProcessor />
         <NavbarWrapper />
