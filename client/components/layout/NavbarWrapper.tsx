bf0ba9a98b43e881247fde9762e7d209bf5744fa
Prince <prince1362005@gmail.com>
2025-07-31 17:06:19 +0530

    udated

--- client/components/layout/NavbarWrapper.tsx
+++ client/components/layout/NavbarWrapper.tsx
@@ -3,6 +3,16 @@ import NavbarClient from "./Navbar";
 
 export default async function NavbarWrapper() {
   const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  const userRaw = await getUser();
+  // Normalize user fields to undefined if null for type compatibility
+  const user = userRaw
+    ? {
+      ...userRaw,
+      email: userRaw.email ?? undefined,
+      given_name: userRaw.given_name ?? undefined,
+      family_name: userRaw.family_name ?? undefined,
+      picture: userRaw.picture ?? undefined,
+    }
+    : null;
   return <NavbarClient user={user} />;
 }
